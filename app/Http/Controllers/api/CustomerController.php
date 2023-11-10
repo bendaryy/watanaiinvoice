cba4fb27cf0f7688eac67463b79f2b065b172122
bendaryy <mohamedbendary1994@gmail.com>
2023-11-10 09:50:00 +0200

    kdsf

--- app/Http/Controllers/api/CustomerController.php
+++ app/Http/Controllers/api/CustomerController.php
@@ -10,20 +10,16 @@ class CustomerController extends Controller
 {
     public function showCustomerName(Request $request)
     {
-        $validator = $request->validate([
-            'name' => 'required',
-            'user_id' => 'required',
-        ]);
-
-         if ($validator->fails()) {
-            return redirect()
-                ->back()
-                ->withErrors($validator)
-                ->withInput(); // Repopulate the form with old input values
-        }
 
         $name = $request->name;
         $user_id = $request->user_id;
+
+         if (!$request->has('name')) {
+            return response()->json(['error' => 'customer name is required'], 400);
+        }
+         if (!$request->has('user_id')) {
+            return response()->json(['error' => 'ownr user is required'], 400);
+        }
         $customerName = Customer::where('name', "LIKE", '%' . $name . '%')->where("user_id", $user_id)->get();
         if (isset($customerName[0])) {
             return $customerName;
